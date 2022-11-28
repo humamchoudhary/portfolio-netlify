12cf158436d1f1880d1972bb178e3903c562a12c
humamchoudhary <70004653+humamchoudhary@users.noreply.github.com>
2022-11-28 12:47:42 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import {
-  BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
@@ -9,21 +8,26 @@ import Home from './components/Home/Home';
 import PortFolioDetails from './components/PortFolioDetails/PortFolioDetails';
 import Error404 from './components/404/404';
 import ProjectDetails from './components/ProJectDetails/ProjectDetails';
+import { HelmetProvider } from 'react-helmet-async'
+
 
 const App = () => {
 
 
   return (
     <>
-      <HashRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path="/Portfolio" element={<PortFolioDetails />} />
-          <Route path='/Projects/:id' element={<ProjectDetails />} />
-          <Route path='/*' element={<Error404 />} />
-        </Routes>
+      <HelmetProvider>
 
-      </HashRouter>
+        <HashRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path="/Portfolio" element={<PortFolioDetails />} />
+            <Route path='/Projects/:id' element={<ProjectDetails />} />
+            <Route path='/*' element={<Error404 />} />
+          </Routes>
+
+        </HashRouter>
+      </HelmetProvider>
 
     </>
   )
