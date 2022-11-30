a93a7194618eb9196fe14456a4947f2befc23e63
humamchoudhary <70004653+humamchoudhary@users.noreply.github.com>
2022-11-30 11:23:49 +0500

    .

--- src/components/More About/Aboutdetails.jsx
+++ src/components/More About/Aboutdetails.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import "./aboutdetails.css"
 import ME from "../../assets/me.jpeg"
 
 const Aboutdetails = () => {
     return (
         <section id="#">
-            <div className="about__me__header">
 
-                <h5>Get to know me</h5>
-                <h1>About me</h1>
-                <h3>I Love technology <br /> and Design</h3>
+            <div className="container about__me__header">
+                <div className="about__me__header__text">
+                    <h1>About me</h1>
+                    <h3>I Love technology <br /> and Design</h3>
+                </div>
+                <div className="about__me__header__img">
+
+                    <img src={ME} alt="About me" />
+                </div>
+
             </div>
         </section>
     )
