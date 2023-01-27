d1500d6790b5725fa0567f836aff81c9fd7dc23e
humamchoudhary <70004653+humamchoudhary@users.noreply.github.com>
2023-01-27 21:52:48 +0500

    Update projects.js

--- src/projects.js
+++ src/projects.js
@@ -10,7 +10,7 @@ export const AllProjects = [
         img2: AlKitab,
         gitlink: "https://github.com/humamchoudhary",
         demolink: "https://github.com/humamchoudhary",
-        details: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente architecto distinctio voluptatum numquam odio blanditiis dolores saepe ullam, accusantium vel velit similique error adipisci doloribus aliquid, sit cupiditate vitae laborum fugiat eveniet possimus. Aut minima velit magni laboriosam, distinctio consectetur. Voluptatum commodi aspernatur sit temporibus molestiae qui nisi necessitatibus culpa?"
+        details: "Al-Kitab is a Quran mobile app that offers a convenient way to read and listen to the Quran on-the-go. The app features a user-friendly interface and allows users to read the Quran by surahs or by ayahs. The audio functionality allows users to listen to the Quran being recited in a clear and melodic voice, making it perfect for those who are learning to read the Quran or for those who prefer listening to the Quran over reading it. Additionally, the app offers multiple translations in various languages, making it accessible for a wide range of users. Overall, Al-Kitab is a valuable tool for anyone looking to deepen their connection with the Quran."
     },
     
     {
@@ -36,7 +36,7 @@ export const Projects = [
         img2: AlKitab,
         gitlink: "https://github.com/humamchoudhary",
         demolink: "https://github.com/humamchoudhary",
-        details: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente architecto distinctio voluptatum numquam odio blanditiis dolores saepe ullam, accusantium vel velit similique error adipisci doloribus aliquid, sit cupiditate vitae laborum fugiat eveniet possimus. Aut minima velit magni laboriosam, distinctio consectetur. Voluptatum commodi aspernatur sit temporibus molestiae qui nisi necessitatibus culpa?"
+        details: "Al-Kitab is a Quran mobile app that offers a convenient way to read and listen to the Quran on-the-go. The app features a user-friendly interface and allows users to read the Quran by surahs or by ayahs. The audio functionality allows users to listen to the Quran being recited in a clear and melodic voice, making it perfect for those who are learning to read the Quran or for those who prefer listening to the Quran over reading it. Additionally, the app offers multiple translations in various languages, making it accessible for a wide range of users. Overall, Al-Kitab is a valuable tool for anyone looking to deepen their connection with the Quran."
     },
     {
         id: 2,
