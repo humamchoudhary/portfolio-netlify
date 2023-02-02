65067a391e2ed3956c81bbc2fd84a9afddf2a392
humamchoudhary <70004653+humamchoudhary@users.noreply.github.com>
2023-02-02 12:31:03 +0500

    update

--- src/projects.js
+++ src/projects.js
@@ -1,77 +1,88 @@
-import AlKitab from "./assets/Al Kitab.png"
-import Swizzle from "./assets/swizzle_proj.png"
-import Vwatch from "./assets/vwatch.png"
+import AlKitab from "./assets/Al Kitab.png";
+import Swizzle from "./assets/swizzle_proj.png";
+import Vwatch from "./assets/vwatch.png";
 export const AllProjects = [
-    {
-        id: 1,
-        name: "Al Kitab",
-        type: "Mobile App",
-        lang: "Flutter",
-        img: AlKitab,
-        img2: AlKitab,
-        gitlink: "https://github.com/humamchoudhary",
-        demolink: "https://github.com/humamchoudhary",
-        details: "Al-Kitab is a Quran mobile app created using the flutter framework. It is designed to provide users with an easy and convenient way to access the Quran on their mobile devices. The app is packed with multiple features that make it a must-have for anyone who wants to read and listen to the Quran. One of the most notable features of Al-Kitab is its reading functionality. Users can choose to read the Quran by surahs or by ayahs. This allows them to read the Quran in a way that suits them best. The app also has an audio functionality that allows users to listen to the Quran while they read. This is especially useful for those who prefer to listen to the Quran while on the go. Another great feature of Al-Kitab is its user-friendly interface. The app is designed to be easy to navigate and understand, making it accessible to users of all ages and backgrounds. It also includes a search function that allows users to quickly find specific surahs or ayahs. In addition to its reading and audio functionality, Al-Kitab also includes a number of other features that make it a comprehensive Quran app. These include the ability to bookmark specific surahs or ayahs, the ability to highlight and annotate text, and the ability to share passages with others. Overall, Al-Kitab is a fantastic Quran app that is packed with features that make it a must-have for anyone who wants to read and listen to the Quran on their mobile device. Whether you're a student of the Quran or simply someone who wants to read and listen to the Quran on the go, Al-Kitab is the perfect app for you."
-    },
-    
-    {
-        id: 2,
-        name: "Swizzle",
-        type: "Static website",
-        lang: "HTML , CSS , JS",
-        img: Swizzle,
-        img2: Swizzle,
-        gitlink: "https://github.com/humamchoudhary/humamchoudhary.github.io/tree/main/swizzl",
-        demolink: "https://humamchoudhary.github.io/swizzl/",
-        details: "Swizzl is a restaurant web app that offers an easy and convenient way to order food online. The app is built using HTML, JavaScript and CSS, making it user-friendly and easy to navigate. The main feature of the app is its menu, which is divided into different sections like Appetizers, Entrees, Desserts and Drinks. Users can browse through the menu, view pictures of the dishes and read detailed descriptions of each product. The app also includes a 'Trending Now' section, where users can see the most popular dishes and products that are currently being ordered. Additionally, the app contains details about the different products including ingredients, nutritional information and pricing. Swizzl allows customers to order online and also includes a section for special requests and comments. Overall, the app makes ordering food an enjoyable and hassle-free experience."
-    },
-    {
-        id: 3,
-        name: "Vwatch",
-        type: "Video On Demand Mobile App",
-        lang: "Python, Flutter",
-        img: Vwatch,
-        img2: Vwatch,
-        gitlink: "https://github.com/humamchoudhary/Vwatch",
-        demolink: "https://github.com/humamchoudhary/Vwatch",
-        details: "V-watch is a Video on demand app, it is created using Flutter, Python (Flask), and JSON database. The aim is to create a beautiful looking mobile app on which the user can have break free experience while watching their favorite movie or TV show. V-Watch is a video on demand (VOD) streaming app that allows users to access a wide variety of movies, television shows, and other video content on their devices. With V-Watch, users can browse and search for content, create and save playlists, and stream videos in high definition."
-    },
+  {
+    id: 1,
+    name: "Al Kitab",
+    type: "Mobile App",
+    lang: "Flutter",
+    subline: "Quran Mobile App",
+    role: "Fullstack developer",
+    img: AlKitab,
+    img2: AlKitab,
+    gitlink: "https://github.com/humamchoudhary",
+    demolink: "https://github.com/humamchoudhary",
+    details:
+      "Al-Kitab is a Quran mobile app created using the flutter framework. It is designed to provide users with an easy and convenient way to access the Quran on their mobile devices. The app is packed with multiple features that make it a must-have for anyone who wants to read and listen to the Quran. One of the most notable features of Al-Kitab is its reading functionality. Users can choose to read the Quran by surahs or by ayahs. This allows them to read the Quran in a way that suits them best. The app also has an audio functionality that allows users to listen to the Quran while they read. This is especially useful for those who prefer to listen to the Quran while on the go. Another great feature of Al-Kitab is its user-friendly interface. The app is designed to be easy to navigate and understand, making it accessible to users of all ages and backgrounds. It also includes a search function that allows users to quickly find specific surahs or ayahs. In addition to its reading and audio functionality, Al-Kitab also includes a number of other features that make it a comprehensive Quran app. These include the ability to bookmark specific surahs or ayahs, the ability to highlight and annotate text, and the ability to share passages with others. Overall, Al-Kitab is a fantastic Quran app that is packed with features that make it a must-have for anyone who wants to read and listen to the Quran on their mobile device. Whether you're a student of the Quran or simply someone who wants to read and listen to the Quran on the go, Al-Kitab is the perfect app for you.",
+  },
 
-]
+  {
+    id: 2,
+    name: "Swizzle",
+    type: "Static website",
+    lang: "HTML , CSS , JS",
+    subline: "Resturent Website",
+    role: "Frontend developer",
+    img: Swizzle,
+    img2: Swizzle,
+    gitlink:
+      "https://github.com/humamchoudhary/humamchoudhary.github.io/tree/main/swizzl",
+    demolink: "https://humamchoudhary.github.io/swizzl/",
+    details:
+      "Swizzl is a restaurant web app that offers an easy and convenient way to order food online. The app is built using HTML, JavaScript and CSS, making it user-friendly and easy to navigate. The main feature of the app is its menu, which is divided into different sections like Appetizers, Entrees, Desserts and Drinks. Users can browse through the menu, view pictures of the dishes and read detailed descriptions of each product. The app also includes a 'Trending Now' section, where users can see the most popular dishes and products that are currently being ordered. Additionally, the app contains details about the different products including ingredients, nutritional information and pricing. Swizzl allows customers to order online and also includes a section for special requests and comments. Overall, the app makes ordering food an enjoyable and hassle-free experience.",
+  },
+  {
+    id: 3,
+    name: "Vwatch",
+    type: "Mobile App",
+    lang: "Python, Flutter",
+    subline: "Video On Demand App",
+    role: "Fullstack developer",
+    img: Vwatch,
+    img2: Vwatch,
+    gitlink: "https://github.com/humamchoudhary/Vwatch",
+    demolink: "https://github.com/humamchoudhary/Vwatch",
+    details:
+      "V-watch is a Video on demand app, it is created using Flutter, Python (Flask), and JSON database. The aim is to create a beautiful looking mobile app on which the user can have break free experience while watching their favorite movie or TV show. V-Watch is a video on demand (VOD) streaming app that allows users to access a wide variety of movies, television shows, and other video content on their devices. With V-Watch, users can browse and search for content, create and save playlists, and stream videos in high definition.",
+  },
+];
 export const Projects = [
-    {
-        id: 1,
-        name: "Al Kitab",
-        type: "Mobile App",
-        lang: "Flutter",
-        img: AlKitab,
-        img2: AlKitab,
-        gitlink: "https://github.com/humamchoudhary",
-        demolink: "https://github.com/humamchoudhary",
-        details:  "Al-Kitab is a Quran mobile app created using the flutter framework. It is designed to provide users with an easy and convenient way to access the Quran on their mobile devices. The app is packed with multiple features that make it a must-have for anyone who wants to read and listen to the Quran. One of the most notable features of Al-Kitab is its reading functionality. Users can choose to read the Quran by surahs or by ayahs. This allows them to read the Quran in a way that suits them best. The app also has an audio functionality that allows users to listen to the Quran while they read. This is especially useful for those who prefer to listen to the Quran while on the go. Another great feature of Al-Kitab is its user-friendly interface. The app is designed to be easy to navigate and understand, making it accessible to users of all ages and backgrounds. It also includes a search function that allows users to quickly find specific surahs or ayahs. In addition to its reading and audio functionality, Al-Kitab also includes a number of other features that make it a comprehensive Quran app. These include the ability to bookmark specific surahs or ayahs, the ability to highlight and annotate text, and the ability to share passages with others. Overall, Al-Kitab is a fantastic Quran app that is packed with features that make it a must-have for anyone who wants to read and listen to the Quran on their mobile device. Whether you're a student of the Quran or simply someone who wants to read and listen to the Quran on the go, Al-Kitab is the perfect app for you."
-    },
-    {
-        id: 2,
-        name: "Swizzle",
-        type: "Static website",
-        lang: "HTML , CSS , JS",
-        img: Swizzle,
-        img2: Swizzle,
-        gitlink: "https://github.com/humamchoudhary/humamchoudhary.github.io/tree/main/swizzl",
-        demolink: "https://humamchoudhary.github.io/swizzl/",
-        details: "Swizzl is a restaurant web app that offers an easy and convenient way to order food online. The app is built using HTML, JavaScript and CSS, making it user-friendly and easy to navigate. The main feature of the app is its menu, which is divided into different sections like Appetizers, Entrees, Desserts and Drinks. Users can browse through the menu, view pictures of the dishes and read detailed descriptions of each product. The app also includes a 'Trending Now' section, where users can see the most popular dishes and products that are currently being ordered. Additionally, the app contains details about the different products including ingredients, nutritional information and pricing. Swizzl allows customers to order online and also includes a section for special requests and comments. Overall, the app makes ordering food an enjoyable and hassle-free experience."
-    },
-    {
-        id: 3,
-        name: "Vwatch",
-        type: "Video On Demand Mobile App",
-        lang: "Python, Flutter",
-        img: Vwatch,
-        img2: Vwatch,
-        gitlink: "https://github.com/humamchoudhary/Vwatch",
-        demolink: "https://github.com/humamchoudhary/Vwatch",
-        details: "V-watch is a Video on demand app, it is created using Flutter, Python (Flask), and JSON database. The aim is to create a beautiful looking mobile app on which the user can have break free experience while watching their favorite movie or TV show. V-Watch is a video on demand (VOD) streaming app that allows users to access a wide variety of movies, television shows, and other video content on their devices. With V-Watch, users can browse and search for content, create and save playlists, and stream videos in high definition."
-    },
-
-
-]
+  {
+    id: 1,
+    name: "Al Kitab",
+    type: "Mobile App",
+    lang: "Flutter",
+    img: AlKitab,
+    img2: AlKitab,
+    gitlink: "https://github.com/humamchoudhary",
+    demolink: "https://github.com/humamchoudhary",
+    details:
+      "Al-Kitab is a Quran mobile app created using the flutter framework. It is designed to provide users with an easy and convenient way to access the Quran on their mobile devices. The app is packed with multiple features that make it a must-have for anyone who wants to read and listen to the Quran. One of the most notable features of Al-Kitab is its reading functionality. Users can choose to read the Quran by surahs or by ayahs. This allows them to read the Quran in a way that suits them best. The app also has an audio functionality that allows users to listen to the Quran while they read. This is especially useful for those who prefer to listen to the Quran while on the go. Another great feature of Al-Kitab is its user-friendly interface. The app is designed to be easy to navigate and understand, making it accessible to users of all ages and backgrounds. It also includes a search function that allows users to quickly find specific surahs or ayahs. In addition to its reading and audio functionality, Al-Kitab also includes a number of other features that make it a comprehensive Quran app. These include the ability to bookmark specific surahs or ayahs, the ability to highlight and annotate text, and the ability to share passages with others. Overall, Al-Kitab is a fantastic Quran app that is packed with features that make it a must-have for anyone who wants to read and listen to the Quran on their mobile device. Whether you're a student of the Quran or simply someone who wants to read and listen to the Quran on the go, Al-Kitab is the perfect app for you.",
+  },
+  {
+    id: 2,
+    name: "Swizzle",
+    type: "Static website",
+    lang: "HTML , CSS , JS",
+    img: Swizzle,
+    img2: Swizzle,
+    gitlink:
+      "https://github.com/humamchoudhary/humamchoudhary.github.io/tree/main/swizzl",
+    demolink: "https://humamchoudhary.github.io/swizzl/",
+    details:
+      "Swizzl is a restaurant web app that offers an easy and convenient way to order food online. The app is built using HTML, JavaScript and CSS, making it user-friendly and easy to navigate. The main feature of the app is its menu, which is divided into different sections like Appetizers, Entrees, Desserts and Drinks. Users can browse through the menu, view pictures of the dishes and read detailed descriptions of each product. The app also includes a 'Trending Now' section, where users can see the most popular dishes and products that are currently being ordered. Additionally, the app contains details about the different products including ingredients, nutritional information and pricing. Swizzl allows customers to order online and also includes a section for special requests and comments. Overall, the app makes ordering food an enjoyable and hassle-free experience.",
+  },
+  {
+    id: 3,
+    name: "Vwatch",
+    type: "Video On Demand Mobile App",
+    lang: "Python, Flutter",
+    img: Vwatch,
+    img2: Vwatch,
+    gitlink: "https://github.com/humamchoudhary/Vwatch",
+    demolink: "https://github.com/humamchoudhary/Vwatch",
+    details:
+      "V-watch is a Video on demand app, it is created using Flutter, Python (Flask), and JSON database. The aim is to create a beautiful looking mobile app on which the user can have break free experience while watching their favorite movie or TV show. V-Watch is a video on demand (VOD) streaming app that allows users to access a wide variety of movies, television shows, and other video content on their devices. With V-Watch, users can browse and search for content, create and save playlists, and stream videos in high definition.",
+  },
+];
